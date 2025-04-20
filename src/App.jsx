66365b3d4c824d8a0553d5a486c1886afef87647
marciabel <alvarezmarciabelen@gmail.com>
2025-04-20 20:59:24 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import backgroundStars from './assets/background-stars.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
